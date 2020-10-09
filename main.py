fdf75418a922fabf229ccbc8a44cc883218e2448
alfred <alfarv@student.chalmers.se>
2020-10-09 14:14:15 +0200

    added ability to use original resolution

--- main.py
+++ main.py
@@ -4,27 +4,47 @@ from modify_pictures import transform_picture_to_bmp
 def main():
     sg.theme('Dark2')
     layout = [
-        [sg.Text('Choose file: ',
-                 font=12),
-         sg.InputText(font=12),
-         sg.FileBrowse('Choose file...', font=12),
+        [
+            sg.Text('Choose file: ',font=12),
+            sg.InputText(font=12),
+            sg.FileBrowse('Choose file...', font=12),
         ],
-        [sg.Submit('Transform', font=12),
-         sg.Cancel('Quit', font=12)]
+        [
+            sg.Checkbox('Use original dimensions', font=12),
+            sg.Text('Width (px): ',font=12),
+            sg.InputText(font=12, size=(7,10)),
+            sg.Text('Height (px): ',font=12),
+            sg.InputText(font=12, size=(7,10)),
+            sg.Text('Scale: ', font=12),
+            sg.InputText(font=12, size=(3, 10))
+         ],
+        [
+            sg.Submit('Transform', font=12),
+            sg.Cancel('Quit', font=12)
+        ]
     ]
 
     window = sg.Window('Affe\'s BMP transformer 1.0', layout)
     while True:# Event Loop
         event, values = window.read()
-        # print(event, values) #debug
+        print(event, values) #debug
         if(event == "Transform"):
-            status = transform_picture_to_bmp(image_path_in=values['Choose file...'])
-            if status:
-                sg.popup('Transform complete!', title='Status', custom_text='Return',any_key_closes=True, font= 12)
-                print('Transform complete!')
-            elif not status:
-                sg.popup('Transform failed.', title='Status', custom_text='Return', any_key_closes=True, font=12)
-                print('Transform failed.')
+            if not values[1]:
+                status = transform_picture_to_bmp(image_path_in=values['Choose file...'], width=values[2], height=values[3], scale=values[4])
+                if status:
+                    sg.popup('Transform complete!', title='Status', custom_text='Return',any_key_closes=True, font= 12)
+                    print('Transform complete! (with specified dimensions)')
+                else:
+                    sg.popup('Transform failed.', title='Status', custom_text='Return', any_key_closes=True, font=12)
+                    print('Transform failed.')
+            if values[1]:
+                status = transform_picture_to_bmp(image_path_in=values['Choose file...'], use_original_dimensions=True, scale=values[4])
+                if status:
+                    sg.popup('Transform complete!', title='Status', custom_text='Return', any_key_closes=True, font=12)
+                    print('Transform complete! (with original dimensions)')
+                else:
+                    sg.popup('Transform failed.', title='Status', custom_text='Return', any_key_closes=True, font=12)
+                    print('Transform failed.')
         if event in (None, 'Exit', 'Quit'):
             break
 
--- modify_pictures.py
+++ modify_pictures.py
@@ -16,7 +16,12 @@ def remove_alpha_layer(img: Image) -> Image:
     else:
         return img
 
-def transform_picture_to_bmp(image_path_in: str, image_path_out: str=False) -> Image:
+def transform_picture_to_bmp(image_path_in: str,
+                             image_path_out: str=False,
+                             width: int=100,
+                             height:int=100,
+                             scale:int='',
+                             use_original_dimensions: bool=False) -> Image:
     """
     Function for transforming the provided picture to bmp and saving it to
     the output path if provided. If not, the transformed image is saved in
@@ -26,12 +31,20 @@ def transform_picture_to_bmp(image_path_in: str, image_path_out: str=False) -> I
     :param image_path_out: Output path to save the transformed image to,
     :return: True if successful transform, False otherwise
     """
+
     if image_path_in:
         # Create an Image object
         img = Image.open(image_path_in)
 
+        if use_original_dimensions:
+            width = img.width
+            height = img.height
+
+        if scale == '':
+            scale = 1
+
         # Resize
-        resizedImage = img.resize((100, 100)) # change this resolution
+        resizedImage = img.resize((int(scale)*int(width), int(scale)*int(height))) # change this resolution
 
         # Save image to disk
         if image_path_out:
